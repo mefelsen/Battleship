7be284958532f89625cbbbba3c2320099535fe57
Blake Morrell <blknm4598@gmail.com>
2019-09-12 23:27:33 -0500

    Add files via upload
    battleExec handles starting screen and user input

---
+++ battleExec.cpp
@@ -0,0 +1,30 @@
+/* -----------------------------------------------------------------------------
+ *
+* File Name: battleExec.cpp
+* Author: Blake Morrell
+* KU ID: 2866535
+* Email Adress: blknm4598@ku.edu
+* Assignment:   EECS-448 Project Battleship
+* Description:  Handles User input for how many ships
+* Date: 9-12-19
+*
+---------------------------------------------------------------------------- */
+#include "Executive.h"
+
+Executive::Executive()
+{
+
+}
+
+Executive::~Executive()
+{
+
+}
+
+void Executive::startMenu()
+{
+  std::cout << "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n"
+            << "Battleship\n"
+            << "Enter the number of ships: ";
+}
+
---
+++ battleExec.h
@@ -0,0 +1,53 @@
+/* -----------------------------------------------------------------------------
+ *
+* File Name: battleExec.cpp
+* Author: Blake Morrell
+* KU ID: 2866535
+* Email Adress: blknm4598@ku.edu
+* Assignment:   EECS-448 Project Battleship
+* Description:  Handles User input for how many ships
+* Date: 9-12-19
+*
+---------------------------------------------------------------------------- */
+#ifndef EXECUTIVE_H
+#define EXECUTIVE_H
+
+//#include <fstream>
+//#include <limits>
+
+
+class Executive
+{
+public:
+
+    /** Default constructor
+   @pre
+   @post
+   @param None  */
+    Executive();
+
+    /** destructor
+     @pre
+     @post
+     @param None  */
+    ~Executive();
+
+
+    /** function to print the inital starting screen
+     @pre  N/A
+     @post Gives data to Flood class
+     @param None  */
+
+    void startMenu();
+
+private:
+
+      int rows;
+      int cols;
+
+
+
+
+
+};
+#endif
