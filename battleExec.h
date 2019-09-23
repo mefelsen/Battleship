b4478cc555d8c51180b6d75894f94bf1792928fd
Blake Morrell <blknm4598@gmail.com>
2019-09-22 22:50:17 -0500

    Fix comments
    Fix comment style for doxygen

--- battleExec.h
+++ battleExec.h
@@ -1,4 +1,4 @@
-/* -----------------------------------------------------------------------------
+/** -----------------------------------------------------------------------------
  *
 * File Name: battleExec.cpp
 * Author: Blake Morrell
@@ -20,65 +20,65 @@ class Executive
 {
 public:
     /** funtion that runs the program
-    @pre
-    @post runs the program
-    @param None  */
+    *@pre
+    *@post runs the program
+    *@param None  */
     void run();
 
 
     /** Default constructor
-   @pre
-   @post class that houses the functions that will run the program
-   @param None  */
+   *@pre
+   *@post class that houses the functions that will run the program
+   *@param None  */
     Executive();
 
     /** destructor
-     @pre
-     @post
-     @param None  */
+     *@pre
+     *@post
+     *@param None  */
     ~Executive();
 
 
     /** function to print the inital starting screen
-     @pre  N/A
-     @post sets up a game of Battleship
-     @param None  */
+     *@pre  N/A
+     *@post sets up a game of Battleship
+     *@param None  */
     void startMenu();
 
     /** Clears the screen for the next player
-     @pre  N/A
-     @post Empty console screen
-     @param None  */
+     *@pre  N/A
+     *@post Empty console screen
+     *@param None  */
     void ClearScreen();
 
     /**
-     @pre
-     @post checks if the input is valid
-     @param takes a string **/
+     *@pre
+     *@post checks if the input is valid
+     *@param takes a string */
     bool inputChecker(std::string x);
 
     /**
-     @pre
-     @post takes a string input, parses and transforms into coordinates for the board
-     @param takes in a string **/
+     *@pre
+     *@post takes a string input, parses and transforms into coordinates for the board
+     *@param takes in a string */
     bool transfor(std::string x);
 
     /**
-     @pre
-     @post transforms the char into coordinates for the board
-     @param takes in a char **/
+     *@pre
+     *@post transforms the char into coordinates for the board
+     *@param takes in a char */
     bool transtoint(char x);
 
     /**
-     @pre
-     @post transforms the string int into an int which is coordinates for the board
-     @param takes an int **/
+     *@pre
+     *@post transforms the string int into an int which is coordinates for the board
+     *@param takes an int */
     bool transfromchar(int x);
 
     /** Calculated number of hits needed to win
-     @pre ship_num must be defined
-     @post updates win_hits from 0 to the total number of hits needed to win
-     @param ship_num is the number of ships each player has. used to calcualte total hits needed to win **/
+     *@pre ship_num must be defined
+     *@post updates win_hits from 0 to the total number of hits needed to win
+     *@param ship_num is the number of ships each player has. used to calcualte total hits needed to win */
     void CalculateWinHits(int ship_num);
 
 
--- board.h
+++ board.h
@@ -1,4 +1,4 @@
-/* -----------------------------------------------------------------------------
+/** -----------------------------------------------------------------------------
  *
 * File Name: board.h
 * Author: Matt Felsen
@@ -13,75 +13,82 @@
 using namespace std;
 class board {
 public:
-  /* Prints board to console
- @pre
- @post
- @param None  */
+  /** Prints board to console
+ *@pre
+ *@post
+ *@param None  */
   void PrintMap();
 
-  /* Default constructor
+  /** Default constructor
  @pre
  @post
  @param None  */
   board();
 
-  /* Checks if the position does not exceed board boundary
- @pre none
- @post returns true if ship placement is valid, false if not valid
- @param x,y are position. x is horizontal position, y is vertical position,
+  /** Checks if the position does not exceed board boundary
+ *@pre none
+ *@post returns true if ship placement is valid, false if not valid
+ *@param x,y are position. x is horizontal position, y is vertical position,
  num is the size of the ship in question. z is the orientation (up,down,left,right)
    */
   bool DimensionCheck(int x, int y, int num, string z);
 
-  /* Checks if the position does not run into existing
- @pre none
- @post returns true if ship placement is valid, false if not valid
- @param x,y are position. x is horizontal position, y is vertical position,
+  /** Checks if the position does not run into existing
+ *@pre none
+ *@post returns true if ship placement is valid, false if not valid
+ *@param x,y are position. x is horizontal position, y is vertical position,
  num is the size of the ship in question. z is the orientation (up,down,left,right)
    */
   bool OverlapCheck(int x, int y, int num, string z);
 
-  /* Checks if input values are safe (within the grid)
- @pre none
- @post returns true if valid, false if not valid
- @param x,y are position. x is horizontal position, y is vertical position,
+  /** Checks if input values are safe (within the grid)
+ *@pre none
+ *@post returns true if valid, false if not valid
+ *@param x,y are position. x is horizontal position, y is vertical position,
  num is the size of the ship in question. z is the orientation (up,down,left,right),mark is a flag for a single ship
    */
   bool PlaceShip(int x, int y, string z, int num,char mark);
 
-  /* Places ship on board (changes ~ to A)
- @pre none
- @post none
- @param x,y are position. x is horizontal position, y is vertical position,
+  /** Places ship on board (changes ~ to A)
+ *@pre none
+ *@post none
+ *@param x,y are position. x is horizontal position, y is vertical position,
  num is the size of the ship in question. z is the orientation (up,down,left,right), mark is a flag for a single ship
    */
   void MarkShip(int x,int y,int num, string z,char mark);
 
-  /* Checks for hit
- @pre none
- @post none
- @param x,y are position. x is horizontal position, y is vertical position,
+  /** Checks for hit
+ *@pre none
+ *@post none
+ *@param x,y are position. x is horizontal position, y is vertical position,
     */
   bool Attack(int, int);
 
-  /* Prints the hidden board to console
- @pre
- @post
- @param None  */
+  /** Prints the hidden board to console
+ *@pre
+ *@post
+ *@param None  */
   void PrintGrid();
 
-  /* updates the hidden board to reflect players attack
- @pre
- @post
- @param  the x and y coordinates of the place to update the map
+  /** updates the hidden board to reflect players attack
+ *@pre
+ *@post
+ *@param  the x and y coordinates of the place to update the map
  the bool is for determining if it was a success or not -- "x" or "o"  */
   void update(int,int, bool);
 
+  /** If a player already attempted to hit a targe this is a check to retry
+ *@pre
+ *@post
+ *@param  the x and y coordinates of the place to check if it has a 'o'
+ the bool is for determining if it was a success or not  */
   bool retryCheck(int x, int y);
 
 
 private:
-  /*
+  /**
+  grid = enemy board
+  map = personal board
   */
   char grid[8][8];
   char map[8][8];
