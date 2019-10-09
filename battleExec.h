85ed09ddf3b95413f791fa6b5256c52e7c80703c
Marco Borth <borthmarco@ku.edu>
2019-10-09 12:35:39 -0500

    playerSetup method documentation

--- battleExec.h
+++ battleExec.h
@@ -50,6 +50,10 @@ public:
      *@param None  */
     void startMenu();
 
+    /** function to allow player to setup their board before game starts
+     *@pre  player object is created
+     *@post player will place their ships on their board until all ships are placed
+     *@param takes a player object  */
     void playerSetup(player user);
 
     /** Clears the screen for the next player
