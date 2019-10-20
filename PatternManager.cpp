f7766e830e5169a01a207f4d7e86ab2f9209cd9a
ChaseOmega <chaseomega@gmail.com>
2019-10-19 20:25:22 -0500

    Added Functionality to axis checks

--- PatternManager.cpp
+++ PatternManager.cpp
@@ -39,11 +39,31 @@ void PatternManager::coldWarGoneWrong(int range, string cordinates)
 
 bool PatternManager::checkFirstAxis(char Check)
 {
-	
+	if (Check == 'A' ||
+		Check == 'B' ||
+		Check == 'C' ||
+		Check == 'D' ||
+		Check == 'E' ||
+		Check == 'F' ||
+		Check == 'G' ||
+		Check == 'H')
+		return true;
+	else
+		return false;
 }
 
 
 bool PatternManager::checkSecondAxis(char Check)
 {
-	
+	if (Check == '1' ||
+		Check == '2' ||
+		Check == '3' ||
+		Check == '4' ||
+		Check == '5' ||
+		Check == '6' ||
+		Check == '7' ||
+		Check == '8')
+		return true;
+	else
+		return false;
 }
