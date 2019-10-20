47f9d9709a1e842c586881c6846b40313f54c9c1
ChaseOmega <chaseomega@gmail.com>
2019-10-19 20:22:27 -0500

    Added skeleton for PatternManager

---
+++ PatternManager.cpp
@@ -0,0 +1,49 @@
+#include "PatternManager.h"
+
+
+void PatternManager::bomb(int range, string cordinates)
+{
+	
+}
+
+void PatternManager::cross(int range, string cordinates)
+{
+	
+}
+
+void PatternManager::Xshot(int range, string cordinates)
+{
+	
+}
+
+void PatternManager::horizontal(int range, string cordinates)
+{
+	
+}
+
+void PatternManager::vertical(int range, string cordinates)
+{
+	
+}
+
+void PatternManager::coldWarGoneWrong()
+{
+	
+}
+
+void PatternManager::coldWarGoneWrong(int range, string cordinates)
+{
+	
+}
+
+
+bool PatternManager::checkFirstAxis(char Check)
+{
+	
+}
+
+
+bool PatternManager::checkSecondAxis(char Check)
+{
+	
+}
